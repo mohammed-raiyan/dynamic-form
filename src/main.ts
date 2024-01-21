1aabe6b602634422fb30da79f91b7e3a85607073
uts.ultimateshop.fe <raiyanmohammed690@gmail.com>
2024-01-21 18:57:40 +0530

    add firebase config and host

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
-// import { environment } from './environments/environment';
-import {bootstrapApplication} from "@angular/platform-browser";
-import {AppComponent} from "./app/app.component";
-import {provideRouter} from "@angular/router";
-import {routes} from "./app/route";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { provideHttpClient } from "@angular/common/http";
-import { enableProdMode } from "@angular/core";
+import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/route';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { enableProdMode } from '@angular/core';
 
-// if (environment.production) {
-//   enableProdMode();
-// }
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
-    providers:[
-        provideRouter(routes),provideAnimations(), provideHttpClient()
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()],
+}).catch((err) => console.error(err));
